7bbc66561d24f009d906565af54bfed2b282694a
adam <adam@backpackhealth.com>
2021-01-29 01:56:11 -0500

    feat: player animations

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
@@ -11,25 +11,41 @@ public class Player : MonoBehaviour
     private float _jumpHeight = 12.0f;
 
     private CharacterController _controller;
+    private Animator _anim;
+    private bool _jumping = false;
 
-    // Start is called before the first frame update
     void Start()
     {
         _controller = this.GetComponent<CharacterController>();
+        _anim = GetComponentInChildren<Animator>();
     }
 
-
-    // Update is called once per frame
     void Update()
     {
         if (_controller.isGrounded)
         {
-            float h = Input.GetAxis("Horizontal");
+            if (_jumping)
+            {
+                _jumping = false;
+                _anim.SetBool("Jump", _jumping);
+            }
+
+            float h = Input.GetAxisRaw("Horizontal");
             _direction = new Vector3(0, 0, h) * _speed;
+            _anim.SetFloat("Speed", Mathf.Abs(h));
+
+            if (h != 0)
+            {
+                Vector3 facing = transform.localEulerAngles;
+                facing.y = _direction.z > 0 ? 0 : 180;
+                transform.localEulerAngles = facing;
+            }
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _direction.y += _jumpHeight;
+                _jumping = true;
+                _anim.SetBool("Jump", _jumping);
             }
         }
 
